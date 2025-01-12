e538833bbdd07c7b8558f4e970b4f4ad1edf5848
vittoriomanfriani <vittorio.manfriani@gmail.com>
2025-01-12 17:59:17 +0100

    Update 12-01-2025

--- KSATrun.py
+++ KSATrun.py
@@ -10,11 +10,11 @@ importlib.reload(KSAT)
 
 ## Generate a problem to solve.
 # This generate a K-SAT instance with N=100 variables and M=350 Clauses
-ksat = KSAT.KSAT(1000, 2, 3, seed=45)
+ksat = KSAT.KSAT(200, 800, 3, seed=45)
 
 ## Optimize it.
 best = SimAnn.simann(ksat,
-                     mcmc_steps = 200, anneal_steps =20,
+                     mcmc_steps = 5000, anneal_steps =50,
                      beta0 = 1, beta1 = 10,
                      seed = 41,
                      debug_delta_cost = False) # set to True to enable the check
